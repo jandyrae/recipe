f746028a1b8248b73d7a4636bdb3e91e32f64730
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-10-13 19:27:21 -0700

    swagger implementation

---
+++ swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "Recipe API",
+    description:
+      "Simple API for recipe information.",
+  },
+  host: "http://localhost:8080",
+  schemes: ["http", "https"],
+  
+};
+
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./index"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(async() => {
+    require("./index")
+});
