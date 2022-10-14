f4a59b89da2f6ec0adfada5d1ac40622a4d89ed7
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-10-13 19:25:08 -0700

    index

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const config = require("./config/index");
+const bodyParser = require("body-parser");
+const cors = require("./middleware/opencors");
+const app = express();
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger.json");
+
+const port = config.PORT || 8080;
+const connectDB = require("./models/connectDB");
+
+
+app
+  .use([cors, bodyParser.json()])
+  .use((req, res, next) => {
+    console.log("Time: ", Date.now());
+    next();
+  })
+  .use("/", require("./routes/index"))
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+
+
+app.listen(port, () => {
+  console.log(
+    `Application listening on http://localhost:8080/ or ${port} see API documentation on http://localhost:8080/api-docs/`
+  );
+});
+
+connectDB().catch(console.error);
