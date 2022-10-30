40b88f14402df5f16c40d464e53b8335726d9b32
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-10-29 20:06:22 -0700

    oauth route for new endpoint functions

---
+++ routes/oauth.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+// const app = express();
+
+const router = Router();
+
+const AuthController = require("../controllers/auth");
+
+router.get("/auth", AuthController.login);
+router.get("/oauth-callback", AuthController.callback);
+
+
+// router.use(express.static("static")).use(opencors);
+module.exports = router;
