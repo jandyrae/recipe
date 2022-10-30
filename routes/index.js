eb11b74ef28e4b9bb7cd4343f1fafe5bde6b9b8f
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-10-29 20:09:12 -0700

    routes index file adjustments to move through auth needs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const router = express();
-const opencors = require('../middleware/opencors');
 
-router.use(opencors);
+const express = require("express");
+const router = express.Router();
+var path = require("path");
+const AuthController = require("../controllers/auth");
+const opencors = require("../middleware/opencors");
+
+router.use([opencors, express.json()]);
+router.use("/", AuthController.load);
 router.use("/recipes", require("./recipes"));
-router.use("/fromTheKitchenOf", require("./kitchens"))
+router.use("/fromTheKitchenOf", require("./kitchens"));
 router.use("/api-docs", require("./swagger"));
 
 module.exports = router;
