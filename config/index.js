1eb9ed64ab3f777341367a926877639acb13e47f
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-10-29 20:08:36 -0700

    updated config file to better hide env variables

--- config/index.js
+++ config/index.js
@@ -1,6 +1,14 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const mongoUrl = process.env.DB_URI;
+const renderAPI = process.env.RENDER_APIKEY;
+const dbName = process.env.DB_NAME;
+const githubClientUrl = process.env.GITHUB_CLIENT_ID;
+const githubSecret = process.env.GITHUB_SECRET;
+const redirect_uri = process.env.REDIRECT;
+const client_id = process.env.CLIENT_ID;
+
 module.exports = {
-  dotenv,
+  mongoUrl, renderAPI, dbName, githubClientUrl, githubSecret, dotenv, redirect_uri, client_id
 };
