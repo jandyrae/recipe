cd811bda455168e315465102c2f1c30f5211c90f
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-11-02 23:12:00 -0700

    added loadUser to endpoint / route

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,31 @@
-
+const config = require("../config/index");
 const express = require("express");
 const router = express.Router();
-
-const AuthController = require("../controllers/auth");
+const { auth } = require("express-openid-connect");
+const {AuthController} = require("../controllers/auth");
+var path = require("path");
 const opencors = require("../middleware/opencors");
+const loadUser = require("../middleware/loadUser");
+// router.use(express.static("static")).use(opencors);
+
+// const configure = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: config.auth0_secret,
+//     baseURL: config.auth0_baseURL,
+//     clientID: config.auth0_id,
+//     issuerBaseURL: config.auth0_baseURL,
+//   }
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+// router.use(auth(configure));
 
 router.use([opencors, express.json()]);
-router.use("/", AuthController.load);
+// router.use("/authorization", (res,req) => {
+//     res.sendFile(path.join(__dirname, "../static/oauth.html"))});
+router.use("/authorization", require("./oauth"));
 router.use("/recipes", require("./recipes"));
-router.use("/fromTheKitchenOf", require("./kitchens"));
+router.use("/fromTheKitchenOf", loadUser, require("./kitchens"));
 router.use("/api-docs", require("./swagger"));
 
 module.exports = router;
--- routes/oauth.js
+++ routes/oauth.js
@@ -1,12 +1,14 @@
-const { Router } = require("express");
-
+const { Router } = require('express');
 const router = Router();
-
+// const checkJwt = require('../middleware/jwt');
+const {requiresAuth} = require("express-openid-connect");
 const AuthController = require("../controllers/auth");
+const opencors = require('../middleware/opencors');
 
-router.get("/auth", AuthController.login);
-router.get("/oauth-callback", AuthController.callback);
+router.use([opencors]);
+router.get("/login", AuthController.login);
+// moves to authorize then returns
+router.get("/callback", AuthController.callback);
 
 
-// router.use(express.static("static")).use(opencors);
 module.exports = router;
