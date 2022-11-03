6b0e1a7780c9b528a24d5c679992efc4e35fa0f0
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-11-02 23:13:02 -0700

    jwt functions

---
+++ middleware/jwt.js
@@ -0,0 +1,49 @@
+
+const { auth } = require('express-oauth2-jwt-bearer');
+const config = require("../config")
+
+const checkScopes = requiredScopes('read:messages');
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: 'https://dev-ntg5ph6kts8y1iza.us.auth0.com/api/v2/',
+  issuerBaseURL: config.auth0_baseURL,
+});
+
+
+
+module.exports = checkJwt;
+
+
+
+
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://dev-ntg5ph6kts8y1iza.us.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  body: '{"client_id":"iSrlzg4MYkugMzCBy8L3BpqgA4GDdXyc","client_secret":"j-46U7ubveYux7QH0XmlagsuFMFbcPLGsJmtSuejcVjcpD9o0zGtTEM2LRq7cHBb","audience":"https://dev-ntg5ph6kts8y1iza.us.auth0.com/api/v2/","grant_type":"client_credentials"}' };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+const axios = require("axios");
+const { config } = require('dotenv');
+
+const options = { 
+  method: "GET",
+  url: "http://path_to_your_api/",
+  headers: { "authorization": "Bearer TOKEN" },
+};
+
+axios(options)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
