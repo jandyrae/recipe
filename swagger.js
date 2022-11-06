b3d1364f2fa8fa9843ab7a86340d0e8e7de3c1e6
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-11-05 17:51:11 -0700

    added to swagger

--- swagger.js
+++ swagger.js
@@ -18,10 +18,12 @@ const doc = {
   ],
   securityDefinitions: {
     OAuth2: {
-      type: "oauth2",
+      type: "apiKey",
+      // tokenUrl: "https://dev-ntg5ph6kts8y1iza.us.auth0.com/oauth/token",
       name: "Authorization",
       scheme: "bearer",
       in: "header",
+      oauth: []
     }, //https://github.com/davibaltar/swagger-autogen#bearer-auth-example
   },
   definitions: {
